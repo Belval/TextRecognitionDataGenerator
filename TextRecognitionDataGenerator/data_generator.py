9f4859c0a02028dda9ddf531bf21a61e666421ff
MichalLukac <cospelthetraceur@gmail.com>
2019-08-24 19:21:56 +0200

    Update generator

--- TextRecognitionDataGenerator/data_generator.py
+++ TextRecognitionDataGenerator/data_generator.py
@@ -23,7 +23,7 @@ except ImportError as e:
 class FakeTextDataGenerator(object):
     sometimes = lambda aug: iaa.Sometimes(0.5, aug)
     seq = iaa.Sequential([
-            sometimes(iaa.PerspectiveTransform(scale=(0.01, 0.1))),
+            sometimes(iaa.PerspectiveTransform(scale=(0.01, 0.08))),
             sometimes(
                 iaa.OneOf([
                     iaa.CoarseDropout((0.03, 0.05), size_percent=(0.1, 0.3)),
@@ -33,22 +33,32 @@ class FakeTextDataGenerator(object):
                 ])
             ),
             iaa.Multiply((0.3, 1.7), per_channel=0.5),
-            sometimes(iaa.AdditiveGaussianNoise((0.02,0.2))),
+            sometimes(iaa.AdditiveGaussianNoise((0.02, 0.2))),
             sometimes(
                 iaa.OneOf([
-                    iaa.MotionBlur(k=(3,5),angle=(0,360)),
-                    iaa.GaussianBlur((0, 1.5)),
+                    iaa.MotionBlur(k=(3,5),angle=(0, 360)),
+                    iaa.GaussianBlur((0, 1.3)),
                     iaa.AverageBlur(k=(2, 4)),
                     iaa.MedianBlur(k=(3, 7))
                 ])
             ),
             sometimes(
                 iaa.CropAndPad(
-                    percent=(-0.05, 0.1),
+                    percent=(-0.05, 0.15),
                     pad_mode=ia.ALL,
                     pad_cval=(0, 255)
                 ),
-            )
+            ),
+            sometimes(iaa.Add((-50, 50), per_channel=0.5)),
+            sometimes(iaa.AdditivePoissonNoise((0.02,0.1))),
+            sometimes(iaa.ElasticTransformation(alpha=(1.0, 2.0), sigma=(2.0, 3.0))), # move pixels locally around (with random strengths)
+            sometimes(iaa.PiecewiseAffine(scale=(0.01, 0.02))), # sometimes move parts of the image around
+            sometimes(iaa.FrequencyNoiseAlpha(
+                    exponent=(-4, 0),
+                    first=iaa.Multiply((0.8, 1.2), per_channel=0.5),
+                    second=iaa.ContrastNormalization((0.5, 3.0))
+                )
+            ),
         ]
     )
 
