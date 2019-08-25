144dd0943278eb281e20bc47ecc2208b75132a17
MichalLukac <cospelthetraceur@gmail.com>
2019-08-25 08:50:21 +0200

    Update generator

--- TextRecognitionDataGenerator/data_generator.py
+++ TextRecognitionDataGenerator/data_generator.py
@@ -23,6 +23,7 @@ except ImportError as e:
 class FakeTextDataGenerator(object):
     sometimes = lambda aug: iaa.Sometimes(0.5, aug)
     seq = iaa.Sequential([
+            iaa.Multiply((0.6, 3.5), per_channel=0.5),
             sometimes(iaa.PerspectiveTransform(scale=(0.01, 0.08))),
             sometimes(
                 iaa.OneOf([
@@ -32,8 +33,13 @@ class FakeTextDataGenerator(object):
                     iaa.Salt((0.03,0.1))
                 ])
             ),
-            iaa.Multiply((0.3, 1.7), per_channel=0.5),
-            sometimes(iaa.AdditiveGaussianNoise((0.02, 0.2))),
+            iaa.Multiply((0.6, 1.3), per_channel=0.5),
+            sometimes(iaa.FrequencyNoiseAlpha(
+                    exponent=(-4, 0),
+                    first=iaa.Multiply((0.8, 1.2), per_channel=0.5),
+                    second=iaa.ContrastNormalization((0.5, 3.0))
+                )
+            ),
             sometimes(
                 iaa.OneOf([
                     iaa.MotionBlur(k=(3,5),angle=(0, 360)),
@@ -45,20 +51,15 @@ class FakeTextDataGenerator(object):
             sometimes(
                 iaa.CropAndPad(
                     percent=(-0.05, 0.15),
-                    pad_mode=ia.ALL,
+                    pad_mode='constant',
                     pad_cval=(0, 255)
                 ),
             ),
             sometimes(iaa.Add((-50, 50), per_channel=0.5)),
-            sometimes(iaa.AdditivePoissonNoise((0.02,0.1))),
             sometimes(iaa.ElasticTransformation(alpha=(1.0, 2.0), sigma=(2.0, 3.0))), # move pixels locally around (with random strengths)
-            sometimes(iaa.PiecewiseAffine(scale=(0.01, 0.02))), # sometimes move parts of the image around
-            sometimes(iaa.FrequencyNoiseAlpha(
-                    exponent=(-4, 0),
-                    first=iaa.Multiply((0.8, 1.2), per_channel=0.5),
-                    second=iaa.ContrastNormalization((0.5, 3.0))
-                )
-            ),
+            sometimes(iaa.PiecewiseAffine(scale=(0.01, 0.02), mode='constant')), # sometimes move parts of the image around
+            sometimes(iaa.AdditiveGaussianNoise((0.02, 0.2))),
+            sometimes(iaa.AdditivePoissonNoise((0.02,0.1)))
         ]
     )
 
