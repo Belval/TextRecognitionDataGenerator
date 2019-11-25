565bce13c7cb9ac17f678fe0a5e7de0e4c0c33d7
Edouard Belval <edouard@belval.org>
2019-11-24 21:50:04 -0500

    Version 1.3.1

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(path.join(here, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="trdg",
-    version="1.2.0",
+    version="1.3.1",
     description="TextRecognitionDataGenerator: A synthetic data generator for text recognition",
     long_description=long_description,
     long_description_content_type="text/markdown",
