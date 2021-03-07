0c876b4a5a11b232a8e8a749a43c3272a14f1086
Abdur Rahman <66300465+abdur75648@users.noreply.github.com>
2021-03-07 19:57:17 +0530

    Add files via upload

---
+++ trdg/background_generator.py
@@ -0,0 +1,89 @@
+import cv2
+import math
+import os
+import random as rnd
+import numpy as np
+
+from PIL import Image, ImageDraw, ImageFilter
+
+
+def gaussian_noise(height, width):
+    """
+        Create a background with Gaussian noise (to mimic paper)
+    """
+
+    # We create an all white image
+    image = np.ones((height, width)) * 255
+
+    # We add gaussian noise
+    cv2.randn(image, 235, 10)
+
+    return Image.fromarray(image).convert("RGBA")
+
+
+def plain_white(height, width):
+    """
+        Create a plain white background
+    """
+
+    return Image.new("L", (width, height), 255).convert("RGBA")
+
+
+def quasicrystal(height, width):
+    """
+        Create a background with quasicrystal (https://en.wikipedia.org/wiki/Quasicrystal)
+    """
+
+    image = Image.new("L", (width, height))
+    pixels = image.load()
+
+    frequency = rnd.random() * 30 + 20  # frequency
+    phase = rnd.random() * 2 * math.pi  # phase
+    rotation_count = rnd.randint(10, 20)  # of rotations
+
+    for kw in range(width):
+        y = float(kw) / (width - 1) * 4 * math.pi - 2 * math.pi
+        for kh in range(height):
+            x = float(kh) / (height - 1) * 4 * math.pi - 2 * math.pi
+            z = 0.0
+            for i in range(rotation_count):
+                r = math.hypot(x, y)
+                a = math.atan2(y, x) + i * math.pi * 2.0 / rotation_count
+                z += math.cos(r * math.sin(a) * frequency + phase)
+            c = int(255 - round(255 * z / rotation_count))
+            pixels[kw, kh] = c  # grayscale
+    return image.convert("RGBA")
+
+
+def image(height, width, image_dir):
+    """
+        Create a background with a image
+    """
+    images = os.listdir(image_dir)
+
+    if len(images) > 0:
+        pic = Image.open(
+            os.path.join(image_dir, images[rnd.randint(0, len(images) - 1)])
+        )
+
+        if pic.size[0] < width:
+            pic = pic.resize(
+                [width, int(pic.size[1] * (width / pic.size[0]))], Image.ANTIALIAS
+            )
+        if pic.size[1] < height:
+            pic = pic.resize(
+                [int(pic.size[0] * (height / pic.size[1])), height], Image.ANTIALIAS
+            )
+
+        if pic.size[0] == width:
+            x = 0
+        else:
+            x = rnd.randint(0, pic.size[0] - width)
+        if pic.size[1] == height:
+            y = 0
+        else:
+            y = rnd.randint(0, pic.size[1] - height)
+
+        return pic.crop((x, y, x + width, y + height))
+    else:
+        raise Exception("No images where found in the images folder!")
---
+++ trdg/data_generator.py
@@ -0,0 +1,252 @@
+import os
+import random as rnd
+
+from PIL import Image, ImageFilter
+
+from trdg import computer_text_generator, background_generator, distorsion_generator
+
+try:
+    from trdg import handwritten_text_generator
+except ImportError as e:
+    print("Missing modules for handwritten text generation.")
+
+
+class FakeTextDataGenerator(object):
+    @classmethod
+    def generate_from_tuple(cls, t):
+        """
+            Same as generate, but takes all parameters as one tuple
+        """
+
+        cls.generate(*t)
+
+    @classmethod
+    def generate(
+        cls,
+        index,
+        text,
+        font,
+        out_dir,
+        size,
+        extension,
+        skewing_angle,
+        random_skew,
+        blur,
+        random_blur,
+        background_type,
+        distorsion_type,
+        distorsion_orientation,
+        is_handwritten,
+        name_format,
+        width,
+        alignment,
+        text_color,
+        orientation,
+        space_width,
+        character_spacing,
+        margins,
+        fit,
+        output_mask,
+        word_split,
+        image_dir,
+        stroke_width=0, 
+        stroke_fill="#282828",
+        image_mode="RGB", 
+    ):
+        image = None
+
+        margin_top, margin_left, margin_bottom, margin_right = margins
+        horizontal_margin = margin_left + margin_right
+        vertical_margin = margin_top + margin_bottom
+
+        ##########################
+        # Create picture of text #
+        ##########################
+        if is_handwritten:
+            if orientation == 1:
+                raise ValueError("Vertical handwritten text is unavailable")
+            image, mask = handwritten_text_generator.generate(text, text_color)
+        else:
+            image, mask = computer_text_generator.generate(
+                text,
+                font,
+                text_color,
+                size,
+                orientation,
+                space_width,
+                character_spacing,
+                fit,
+                word_split,
+                stroke_width, 
+                stroke_fill,
+            )
+        random_angle = rnd.randint(0 - skewing_angle, skewing_angle)
+
+        rotated_img = image.rotate(
+            skewing_angle if not random_skew else random_angle, expand=1
+        )
+
+        rotated_mask = mask.rotate(
+            skewing_angle if not random_skew else random_angle, expand=1
+        )
+
+        #############################
+        # Apply distorsion to image #
+        #############################
+        if distorsion_type == 0:
+            distorted_img = rotated_img  # Mind = blown
+            distorted_mask = rotated_mask
+        elif distorsion_type == 1:
+            distorted_img, distorted_mask = distorsion_generator.sin(
+                rotated_img,
+                rotated_mask,
+                vertical=(distorsion_orientation == 0 or distorsion_orientation == 2),
+                horizontal=(distorsion_orientation == 1 or distorsion_orientation == 2),
+            )
+        elif distorsion_type == 2:
+            distorted_img, distorted_mask = distorsion_generator.cos(
+                rotated_img,
+                rotated_mask,
+                vertical=(distorsion_orientation == 0 or distorsion_orientation == 2),
+                horizontal=(distorsion_orientation == 1 or distorsion_orientation == 2),
+            )
+        else:
+            distorted_img, distorted_mask = distorsion_generator.random(
+                rotated_img,
+                rotated_mask,
+                vertical=(distorsion_orientation == 0 or distorsion_orientation == 2),
+                horizontal=(distorsion_orientation == 1 or distorsion_orientation == 2),
+            )
+
+        ##################################
+        # Resize image to desired format #
+        ##################################
+
+        # Horizontal text
+        if orientation == 0:
+            new_width = int(
+                distorted_img.size[0]
+                * (float(size - vertical_margin) / float(distorted_img.size[1]))
+            )
+            resized_img = distorted_img.resize(
+                (new_width, size - vertical_margin), Image.ANTIALIAS
+            )
+            resized_mask = distorted_mask.resize((new_width, size - vertical_margin), Image.NEAREST)
+            background_width = width if width > 0 else new_width + horizontal_margin
+            background_height = size
+        # Vertical text
+        elif orientation == 1:
+            new_height = int(
+                float(distorted_img.size[1])
+                * (float(size - horizontal_margin) / float(distorted_img.size[0]))
+            )
+            resized_img = distorted_img.resize(
+                (size - horizontal_margin, new_height), Image.ANTIALIAS
+            )
+            resized_mask = distorted_mask.resize(
+                (size - horizontal_margin, new_height), Image.NEAREST
+            )
+            background_width = size
+            background_height = new_height + vertical_margin
+        else:
+            raise ValueError("Invalid orientation")
+
+        #############################
+        # Generate background image #
+        #############################
+        if background_type == 0:
+            background_img = background_generator.gaussian_noise(
+                background_height, background_width
+            )
+        elif background_type == 1:
+            background_img = background_generator.plain_white(
+                background_height, background_width
+            )
+        elif background_type == 2:
+            background_img = background_generator.quasicrystal(
+                background_height, background_width
+            )
+        else:
+            background_img = background_generator.image(
+                background_height, background_width, image_dir
+            )
+        background_mask = Image.new(
+            "RGB", (background_width, background_height), (0, 0, 0)
+        )
+
+        #############################
+        # Place text with alignment #
+        #############################
+
+        new_text_width, _ = resized_img.size
+
+        if alignment == 0 or width == -1:
+            background_img.paste(resized_img, (margin_left, margin_top), resized_img)
+            background_mask.paste(resized_mask, (margin_left, margin_top))
+        elif alignment == 1:
+            background_img.paste(
+                resized_img,
+                (int(background_width / 2 - new_text_width / 2), margin_top),
+                resized_img,
+            )
+            background_mask.paste(
+                resized_mask,
+                (int(background_width / 2 - new_text_width / 2), margin_top),
+            )
+        else:
+            background_img.paste(
+                resized_img,
+                (background_width - new_text_width - margin_right, margin_top),
+                resized_img,
+            )
+            background_mask.paste(
+                resized_mask,
+                (background_width - new_text_width - margin_right, margin_top),
+            )
+
+        #######################
+        # Apply gaussian blur #
+        #######################
+
+        gaussian_filter = ImageFilter.GaussianBlur(
+            radius=blur if not random_blur else rnd.randint(0, blur)
+        )
+        final_image = background_img.filter(gaussian_filter)
+        final_mask = background_mask.filter(gaussian_filter)
+        
+        ############################################
+        # Change image mode (RGB, grayscale, etc.) #
+        ############################################
+        
+        final_image = final_image.convert(image_mode)
+        final_mask = final_mask.convert(image_mode) 
+
+        #####################################
+        # Generate name for resulting image #
+        #####################################
+        # We remove spaces if space_width == 0
+        if space_width == 0:
+            text = text.replace(" ", "")
+        if name_format == 0:
+            image_name = "{}_{}.{}".format(text, str(index), extension)
+            mask_name = "{}_{}_mask.png".format(text, str(index))
+        elif name_format == 1:
+            image_name = "{}_{}.{}".format(str(index), text, extension)
+            mask_name = "{}_{}_mask.png".format(str(index), text)
+        elif name_format == 2:
+            image_name = "{}.{}".format(str(index), extension)
+            mask_name = "{}_mask.png".format(str(index))
+        else:
+            print("{} is not a valid name format. Using default.".format(name_format))
+            image_name = "{}_{}.{}".format(text, str(index), extension)
+            mask_name = "{}_{}_mask.png".format(text, str(index))
+
+        # Save the image
+        if out_dir is not None:
+            final_image.save(os.path.join(out_dir, image_name))
+            if output_mask == 1:
+                final_mask.save(os.path.join(out_dir, mask_name))
+        else:
+            if output_mask == 1:
+                return final_image, final_mask
+            return final_image
