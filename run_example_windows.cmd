bb7b0c828224206e4ef9694dbc85f8777ebe737c
Hashem Zavvari <hashemzawary@gmail.com>
2021-09-08 15:16:45 +0430

    Update run_example_windows.cmd

--- run_example_windows.cmd
+++ run_example_windows.cmd
@@ -1,1 +1,1 @@
-python trdg\run.py -c 10 -l "fa" -e "png" --background 3 --name_format 0 --stroke_width 1 -i testtexts.txt
+python trdg\run.py -f 45 -c 20 -w 3 -r -bl 1 -rbl -l "fa" -e "png" --background 2 --name_format 0 --stroke_width 1 --skew_angle 2 -rk --distorsion 3
