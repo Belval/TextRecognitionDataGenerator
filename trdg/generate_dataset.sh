38a50238570fe4e0c959b56de68ec26987b79429
Nikita Mramorov <nikita_mramorov@epam.com>
2022-01-26 14:26:34 +0300

    Added script for dataset generation

---
+++ trdg/generate_dataset.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+rm -rf faults
+mkdir faults
+python3 run.py -na 2 -b 3 -rs -c 1000000 --output_dir train
+rm -rf faults
+mkdir faults
+python3 run.py -na 2 -b 3 -rs -c 100 --output_dir val
+
+rm -rf faults
+mkdir faults
+
+python3 run.py -na 2 -b 3- rs -c 1000 --output_dir test
+
+zip dataset.zip -r train val test
+
